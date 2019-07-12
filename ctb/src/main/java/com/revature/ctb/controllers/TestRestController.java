9992e328219721b44e8b836f24c3172915073338
Jose Arriaga <josearriag@hotmail.com>
2019-07-12 11:32:31 -0400

    project updated

---
+++ ctb/src/main/java/com/revature/ctb/controllers/TestRestController.java
@@ -0,0 +1,18 @@
+package com.revature.ctb.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestRestController {
+
+	@GetMapping("/test")
+	@ResponseStatus(code = HttpStatus.OK)
+	public String get() {
+		return "hello";
+	}
+}
