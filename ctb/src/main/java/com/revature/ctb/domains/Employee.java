c157547147c16d9a381aaf395544766a76521f15
Jose Arriaga <josearriag@hotmail.com>
2019-07-16 08:59:16 -0400

    Finished some domain classes. And improved validation process.

--- ctb/src/main/java/com/revature/ctb/domains/Employee.java
+++ ctb/src/main/java/com/revature/ctb/domains/Employee.java
@@ -3,6 +3,7 @@ package com.revature.ctb.domains;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -72,6 +73,10 @@ public class Employee {
 	@OneToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "employee_role", joinColumns = @JoinColumn(name = "employee_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private List<Role> roles = new ArrayList<>();
+
+	// One to many relationship (One employee can have many cars)
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.MERGE, mappedBy = "employee")
+	private List<Car> cars = new ArrayList<>();
 
 	public Employee() {
 		super();
--- ctb/src/main/java/com/revature/ctb/restcontrollers/EmployeeRestController.java
+++ ctb/src/main/java/com/revature/ctb/restcontrollers/EmployeeRestController.java
@@ -4,7 +4,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,9 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.ctb.domains.Employee;
 import com.revature.ctb.services.EmployeeService;
-import com.revature.ctb.utils.ValidationUtil;
 
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
@@ -25,22 +23,21 @@ public class EmployeeRestController {
 	private EmployeeService employeeServ; // injecting employeeService
 
 	/**
-	 * This method will register the employee in the system. This is going to be used by
-	 * Angular UI.
-	 * @param emp Employee to register, JSON format object, coming from Angular
-	 * @param theBindingResult Handle by spring and validation framework
+	 * This method will register the employee in the system. This is going to be
+	 * used by Angular UI.
+	 * 
+	 * @param emp              Employee to register, JSON format object, coming from
+	 *                         Angular
+	 *                         
 	 * @return the employee with id following rest API practices
 	 */
 	@PostMapping("employee") // access this using: localhost:8080/api/employee <- GET method
 	@ResponseStatus(code = HttpStatus.CREATED)
-	public Employee postEmployee(@Valid @RequestBody Employee emp, BindingResult theBindingResult) {
-
-		// This method validates the entity (static method)
-		ValidationUtil.checkModelForValidationErrors(theBindingResult);
+	public Employee postEmployee(@Valid @RequestBody Employee emp) {
 
 		// saving employee using the service
 		employeeServ.registerEmployee(emp);
-		
+
 		// hidding employee password
 		emp.setPassword("************");
 
