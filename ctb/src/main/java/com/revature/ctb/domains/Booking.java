ab8b4ebcbcbebe8fbae7f7c41871f863afd6d8c7
TaroMilkTea95 <tanho8577@gmail.com>
2019-07-16 20:28:23 -0400

    booking

--- ctb/src/main/java/com/revature/ctb/domains/Booking.java
+++ ctb/src/main/java/com/revature/ctb/domains/Booking.java
@@ -1,5 +1,64 @@
 package com.revature.ctb.domains;
 
-public class Booking {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "booking")
+public class Booking {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "booking_id")
+	private Integer bookingId;
+	
+	public Booking() {
+	}
+	
+	public Booking(Integer bookingId) {
+	super();
+	}
+
+	public Integer getBookingId() {
+		return bookingId;
+	}
+
+	public void setBookingId(Integer bookingId) {
+		this.bookingId = bookingId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((bookingId == null) ? 0 : bookingId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Booking other = (Booking) obj;
+		if (bookingId == null) {
+			if (other.bookingId != null)
+				return false;
+		} else if (!bookingId.equals(other.bookingId))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Booking [bookingId=" + bookingId + "]";
+	}
+	
 }
