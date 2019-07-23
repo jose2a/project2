0bc2bee4c9c8612a1e8a2b2af57cd20ae9babcd6
Aisha-Hodge <aisha.a.hodge@gmail.com>
2019-07-23 18:22:02 -0400

    ride, infoReq, feedback service

---
+++ ctb/ctb-ng/ctb-ng/src/app/services/info-req.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { appConfig } from '../configs/app.config';
+import { Observable, of } from 'rxjs';
+import { InfoReq } from '../models/infoReq';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoReqService {
+
+  private infoReqUrl = `${appConfig.urlApi}/information`;
+
+  constructor(private http: HttpClient) { }
+
+
+   /*GET all information*/
+   getInformation(): Observable <InfoReq[]> {
+    return this.http.get<InfoReq[]>(this.infoReqUrl);
+  }
+
+   /*POST infoReq from server*/
+   createInfoReq(infoReq: InfoReq): Observable <InfoReq> {
+    return this.http.post<InfoReq>(this.infoReqUrl, infoReq, httpOptions)
+  }
+
+  /*PUT infoReq --update*/
+  updateInfoReq(infoReq: InfoReq): Observable<InfoReq> {
+    const url = `${this.infoReqUrl}/${infoReq.infoReqId}`;
+    return this.http.put<InfoReq>(this.infoReqUrl, infoReq, httpOptions);
+  }
+
+  /*PUT infoReq -- confirm*/
+  confirmRequestReceived(infoReqId: number){
+    const url = `${this.infoReqUrl}/${infoReqId}/confirmed`;
+    return this.http.put<InfoReq>(this.infoReqUrl, infoReq, httpOptions);
+  }
+
+  /*GET infoReq by employeeID*/
+  getReqById(employeeId: number): Observable <InfoReq[]>{
+    return this.http.get<InfoReq[]>(`${this.infoReqUrl}/${employee}/${employeeId}`);
+  }
+
+  /*GET infoReq by ID*/
+  getInfoReqById(infoReqId : number): Observable <Ride> {
+    return this.http.get<InfoReq>(`${this.infoReqUrl}/${infoReqId}`);
+  }
+
+}
--- ctb/ctb-ng/ctb-ng/src/app/services/ride.service.ts
+++ ctb/ctb-ng/ctb-ng/src/app/services/ride.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 import { appConfig } from '../configs/app.config';
-import { Observable, of } from 'rxjx';
+import { Observable, of } from 'rxjs';
 import { Ride } from '../models/ride';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +19,41 @@ export class RideService {
 
   constructor(private http: HttpClient) { }
 
-  
+/*GET rides from server*/
+    getRides(): Observable <Ride[]> {
+  return this.http.get<Ride[]>(this.rideUrl);
+}
+
+  /*POST ride from server*/
+    createRide(ride: Ride): Observable <Ride> {
+      return this.http.post<Ride>(this.rideUrl, ride, httpOptions);
+    }
+
+  /*PUT ride --update*/
+  updateRide(ride: Ride): Observable<Ride> {
+    const url =  `${this.rideUrl}/${ride.rideId}`;
+    return this.http.put<Ride>(this.rideUrl, ride, httpOptions);
+  }
+
+  /*PUT ride --cancel*/
+  softCancelRide(ride: Ride): Observable<Ride> {
+    const url = `${this.rideUrl}/${ride.rideId}/cancel`;
+    return this.http.put<Ride>(this.rideUrl, ride, httpOptions);
+  }
+
+   /*GET employee rides*/
+   getAvailableRides(employeeId: number): Observable <Ride[]> {
+    return this.http.get<Ride[]>(`${this.rideUrl}/available`);
+   }
+
+
+  /*GET ride with ID*/
+    getRideById(rideId: number): Observable <Ride> {
+    return this.http.get<Ride>(`${this.rideUrl}/${rideId}`);
+  }
+
+  /*POST message to passengers*/
+  postMessage(message : any): Observable <void> {
+    return this.http.post<void>(`${this.rideUrl}/message`, message, httpOptions);
+  }
 }
