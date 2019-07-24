c9f4437256bf686ebe3a03559e6b63a56e8fdf41
Aisha-Hodge <aisha.a.hodge@gmail.com>
2019-07-24 16:58:59 -0400

    login update

--- ctb/ctb-ng/ctb-ng/src/app/components/form-employee/form-employee.component.ts
+++ ctb/ctb-ng/ctb-ng/src/app/components/form-employee/form-employee.component.ts
@@ -42,23 +42,4 @@ export class FormEmployeeComponent implements OnInit {
     );
   }
 
-  loginEmployee(): void {
-
-    this.employeeService.loginEmployee(this.employee)
-    .subscribe((newEmployee: Employee) => {
-      console.log(newEmployee);
-
-      this.valErrors = [];
-      this.employee = new Employee();
-
-      this.result = `Employee registered`;
-
-    }, (errorResp: HttpErrorResponse) => {
-        this.result = ``;
-
-        this.valErrors = errorResp.error.errorMessages;
-      }
-    );
-  }
-
 }
