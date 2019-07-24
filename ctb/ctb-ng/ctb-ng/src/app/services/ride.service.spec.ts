24906a5441e11bdf8ddd76da0e8aba4a486d60de
Jose Arriaga <josearriag@hotmail.com>
2019-07-23 20:32:16 -0400

    fixed ride component

--- ctb/ctb-ng/ctb-ng/src/app/services/ride.service.spec.ts
+++ ctb/ctb-ng/ctb-ng/src/app/services/ride.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { RideService } from './rideService';
+import { RideService } from './ride.service';
 
 describe('RideService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
